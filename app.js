520b585fa6bf21c20d096db0bcddb96ad2d4f2a0
nchnsn <nchnsn@gmail.com>
2017-06-06 15:57:30 -0700

    Got flipping working, but found issue when piece is next to two potential flippable pieces.

--- app.js
+++ app.js
@@ -53,18 +53,14 @@ var renderView = function(x, y, color){
     var pieceNode = document.createElement("a");
     pieceNode.classList.add("piece", color);
     cell.appendChild(pieceNode);
+}
 
-    // cell.classList.add('piece', color);
-    /*
-    scoreBoard.board.forEach(function(element){
-        element.forEach(function(element, index){
-            if(element === 'white'){
-                var temp = getElementByTagName("table");
-              //  temp.children[0].class('');
-            }
-        });
-    });
-    */
+var renderViewFlip = function(x, y, color){
+    var colorToRemove = color === 'black' ? 'white' : 'black';
+     document.getElementsByTagName("table")[0].children[0].children[y].children[x].children[0].classList.remove(colorToRemove);
+     document.getElementsByTagName("table")[0].children[0].children[y].children[x].children[0].classList.add(color)
+    var row = document.getElementsByTagName("table")[0].children[0].children[y];
+    var cell = document.getElementsByTagName("table")[0].children[0].children[y].children[x];
 }
 
 
@@ -109,7 +105,7 @@ var addPiece = function(turn, x, y){
     
     scoreBoard.turn === 'white' ? scoreBoard.turn = 'black' : scoreBoard.turn = 'white';
     document.getElementById('turn').innerHTML = scoreBoard.turn;
-    checkNeighbor();
+    checkNeighbor(x,y);
     
 } 
 
@@ -123,6 +119,7 @@ var addPieceAuto = function(){
     renderView(x, y, scoreBoard.turn);
     scoreBoard.turn === 'white' ? scoreBoard.turn = 'black' : scoreBoard.turn = 'white';
     document.getElementById('turn').innerHTML = scoreBoard.turn;
+    console.log(checkAllNeighbors(x,y));
 }
 
 var addPieceManual = function(turn, x, y){
@@ -133,13 +130,199 @@ var addPieceManual = function(turn, x, y){
 }
     
 
-// check neighbor
+// *** FLIP / CHECK NEIGHBORS ***
+
     // check if next piece is opp color
-var checkNeighbor = function(){
-    console.log('checkingggg');
-    var x = $(this).index();
-    console.log(x);
+var checkNeighbor = function(x,y){
+    var pieceUp = scoreBoard.board[y - 1] ? scoreBoard.board[y - 1][x] : 'NA';
+    var pieceRight = scoreBoard.board[y][x + 1];
+    var pieceDown = scoreBoard.board[y + 1] ? scoreBoard.board[y + 1][x] : 'NA';
+    var pieceLeft = scoreBoard.board[y][x - 1];
+    var neighbors = [pieceUp, pieceRight, pieceDown, pieceLeft];
+    if(neighbors.some(function(element){
+        return element === scoreBoard.turn;
+    })){
+       console.log('lets flipppa');
+    }
+    console.log('up ' + pieceUp);
+    console.log('right ' + pieceRight);
+    console.log('down ' + pieceDown);
+    console.log('left ' + JSON.stringify(pieceLeft));
+    console.log(JSON.stringify(scoreBoard.board));
 }
+
+var checkAllNeighbors = function(x, y, direction){
+    var startingPoint = scoreBoard.board[y][x];
+    var runTracker = [];
+    // check all neighbors
+    var pieceUp = {
+        color: scoreBoard.board[y - 1] ? scoreBoard.board[y - 1][x] : 'NA',
+        x: x,
+        y:y - 1,
+        flip: scoreBoard.board[y - 1] ? scoreBoard.board[y - 1][x] === scoreBoard.turn : false,
+        direction: 'up'
+    }
+    
+
+    var pieceDown = {
+        color: scoreBoard.board[y + 1] ? scoreBoard.board[y + 1][x] : 'NA',
+        x: x,
+        y:y + 1,
+        flip: scoreBoard.board[y + 1] ? scoreBoard.board[y + 1][x] === scoreBoard.turn : false,
+        direction: 'down'
+    }
+    var pieceRight = {
+        color: scoreBoard.board[x + 1] ? scoreBoard.board[y][x + 1] : 'NA',
+        x: x + 1,
+        y:y,
+        flip: scoreBoard.board[x + 1] ? scoreBoard.board[y][x + 1] === scoreBoard.turn : false,
+        direction: 'right'
+    }
+    var pieceLeft = {
+        color: scoreBoard.board[x - 1] ? scoreBoard.board[y][x - 1] : 'NA',
+        x: x - 1,
+        y:y,
+        flip: scoreBoard.board[x - 1] ? scoreBoard.board[y][x - 1] === scoreBoard.turn : false,
+        direction: 'left'
+    }
+    var neighbors = [pieceUp, pieceRight, pieceDown, pieceLeft];
+
+    neighbors.forEach(function(element){
+        if(element.color === scoreBoard.turn){
+            console.log('DIRECTION ' + element.direction);
+            console.log('yes!');
+            console.log(element);
+            initRun(element.x, element.y, element.direction);
+            
+        } else {
+            console.log('DIRECTION ' + element.direction);
+            console.log('No!');
+            console.log(element);
+        }
+    });
+    
+
+    function initRun(x,y, direction){
+        console.log('start');
+        console.log(startingPoint);
+        runTracker.push([x,y]);
+        console.log(runTracker);
+        var flip = false;
+        var deadEnd = false;
+        var runPieces = 1;
+        console.log(direction);
+
+        if(direction === 'up'){
+            nextPiece = scoreBoard.board[y - 1][x];
+            if(nextPiece === scoreBoard.board[y][x]){
+                console.log('run again');
+                initRun(x, y - 1, direction);
+            } else if(nextPiece === startingPoint){
+                console.log('end run');
+                if(runTracker.length > 0){
+                    console.log('flipp this!');
+                    runTracker.forEach(function(element){
+                        scoreBoard.board[element[1]][element[0]] = startingPoint;
+                        renderViewFlip(element[0], element[1], startingPoint);
+                    });
+                    console.log(scoreBoard.board);
+                }
+            } else {
+                console.log('notthing');
+            }
+        }  else if(direction === 'right'){
+            nextPiece = scoreBoard.board[y][x + 1];
+            console.log(nextPiece);
+            console.log(scoreBoard.board);
+            // check for opp color
+            if(nextPiece === scoreBoard.board[y][x]){
+                console.log('run again');
+                console.log(x);
+                console.log(y);
+                initRun(x + 1, y, direction);
+            // check for end of run
+        } else if(nextPiece === startingPoint){
+                console.log(x);
+                console.log(y);
+                console.log('end run');
+                if(runTracker.length > 0){
+                    console.log('flipp this!');
+                    runTracker.forEach(function(element){
+                        scoreBoard.board[element[1]][element[0]] = startingPoint;
+                        renderViewFlip(element[0], element[1], startingPoint);
+                    });
+                    console.log(scoreBoard.board);
+                }
+            // check for empty piece
+            } else {
+                console.log('notthing dudes');
+                console.log(scoreBoard.board);
+            }
+        } else if (direction === 'down'){
+            nextPiece = scoreBoard.board[y + 1][x];
+            if(nextPiece === scoreBoard.board[y][x]){
+                console.log('call again');
+                runPieces++;
+                console.log(runPieces);
+                initRun(x, y + 1, direction);
+            } else if(nextPiece === startingPoint){
+                console.log('end run');
+                if(runTracker.length > 0){
+                    console.log('flipp this!');
+                    runTracker.forEach(function(element){
+                        scoreBoard.board[element[1]][element[0]] = startingPoint;
+                        renderViewFlip(element[0], element[1], startingPoint);
+                    });
+                    console.log(scoreBoard.board);
+                }
+            } else {
+                console.log('notthing');
+            }
+        } else  if(direction === 'left'){
+            nextPiece = scoreBoard.board[y][x - 1];
+            // check opp color
+            if(nextPiece === scoreBoard.board[y][x]){
+                runPieces++;
+                console.log('run again');
+                initRun(x - 1, y, direction);
+            // check for end of run
+            } else if(nextPiece === startingPoint){
+                console.log('end run');
+                if(runTracker.length > 0){
+                    console.log('flipp this!');
+                    runTracker.forEach(function(element){
+                        scoreBoard.board[element[1]][element[0]] = startingPoint;
+                        renderViewFlip(element[0], element[1], startingPoint);
+                    });
+                    console.log(scoreBoard.board);
+                }
+            // check for empty 
+            } else {
+                console.log('notthing');
+            }
+        }
+    }
+    // end init run
+    
+}
+
+// end checkAllNeighbors
+
+var checkForRun = function(x,y){
+    var shouldExecute = false;
+    // check all neighbor for opposite piece
+    console.log(checkAllNeighbors(x,y));
+        // if match, check for matches only on the opposite side
+            // if same color, repeat check on opposite side
+            // if opp color change should execute to true
+    // check should exectute flag to see if you should execute run
+        // excute run if true
+}
+
+var executeRun = function(){
+
+}
+
     
 
 // flip pieces
@@ -151,8 +334,7 @@ var allCells = document.getElementsByTagName("td");
 for(var i = 0; i < allCells.length; i++){
     var xCor = i < 8 ? i : i % 7;
     var yCor = i < 8 ? 0 : Math.floor(i / 7);
-   // allCells[i].addEventListener("click", function(){addPiece('white', 0, 0)}); 
 }
-$("table").on("click", "td", addPiece);
+$("table").on("click", "td", addPieceAuto);
 startGame();
 
