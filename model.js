66e710b7bad20013e9b5742c4f6a1db731218f1f
nchnsn <nchnsn@gmail.com>
2017-06-26 14:57:17 -0700

    Added more tests, only allows you to make valid moves.

--- model.js
+++ model.js
@@ -1,7 +1,8 @@
 var Othello = function(){
     this.whitesTurn = null;
     this.status = null;
-    this.board = [[null,null,null,null,null,null,null,null],
+    this.board = [
+                [null,null,null,null,null,null,null,null],
                 [null,null,null,null,null,null,null,null],
                 [null,null,null,null,null,null,null,null],
                 [null,null,null,null,null,null,null,null],
@@ -28,14 +29,28 @@ Othello.prototype.currentTurn = function(){
     return this.whitesTurn ? 'white' : 'black';
 }
 
+Othello.prototype.oppositeTurn = function(){
+    return this.whitesTurn ? 'black' : 'white';
+}
+
 Othello.prototype.placePiece = function(row, column){
     this.board[row][column] = this.currentTurn();
+    this.currentTurn() === 'white' ? this.whiteScore++ : this.blackScore++;
     return [ this.board[row][column], row, column];
 }
 
 // helper function for 'flip pieces'
 Othello.prototype.flipPiece = function(row, column){
-    this.board[row][column] = this.currentTurn();
+    console.log('flippin');
+    console.log(this.board[row][column]);
+    console.log(this.oppositeTurn());
+    if(this.board[row][column] === 'white'){
+      this.board[row][column] = 'black';  
+      this.blackScore++ && this.whiteScore--
+    } else {
+        this.board[row][column] = 'white';
+        this.whiteScore++ && this.blackScore--;
+    }
     return [ this.board[row][column], row, column];
 }
 
@@ -43,19 +58,196 @@ Othello.prototype.removePiece = function(row, column){
     this.board[row][column] = null;
     return [ this.board[row][column], row, column]
 }
+// *** carry out flips on each turn ***
+// check each surrounding piece to see if there are potential moves
 
-Othello.prototype.flipPieces = function(row, column){
-    console.log('checking for flipss');
+
+// flips available pieces in a move
+Othello.prototype.flipAvailable = function(row, column){
+    var piecesToFlip = [];
+    var game = this;
+    var neighbors = {
+        up:{
+            value:this.board[row - 1] ? this.board[row - 1][column] : null,
+            direction:'up',
+            row:row - 1,
+            column:column
+            },
+        right:{
+            value:this.board[row][column + 1] ? this.board[row][column + 1] : null ,
+            direction:'right',
+            row:row,
+            column:column + 1
+            },
+        down:{
+            value:this.board[row + 1] ? this.board[row + 1][column] : null,
+            direction:'down',
+            row:row + 1,
+            column:column
+        },
+        left:{
+            value:this.board[row][column - 1] ? this.board[row][column - 1] : null, 
+            direction:'left',
+            row:row,
+            column:column - 1
+        }
+    }
+
+    var checkNeighbors = function(){
+        for(key in neighbors){
+            console.log(key);
+            console.log(neighbors[key].value);
+            if(neighbors[key].value === game.oppositeTurn()){
+                console.log('move here');
+                piecesToFlip.push([neighbors[key].row, neighbors[key].column]);
+                checkMove(key, neighbors[key].row, neighbors[key].column);
+            } else {
+            console.log('no move');
+            }
+        }
+        console.log(piecesToFlip);
+        return piecesToFlip;
+    }    
+    // for each of the potential moves to see if valid 
+    var checkMove = function(direction, row, column){
+        if(direction === 'up'){
+            if(game.board[row - 1][column] === game.oppositeTurn()){
+                piecesToFlip.push([row - 1, column]);
+                checkMove('up', row - 1, column);
+            } else if(game.board[row - 1][column] === game.currentTurn()){
+                piecesToFlip.forEach(function(element){
+                    game.flipPiece(element[0],element[1]);
+                });
+            } 
+        } else if(direction === 'right'){
+             if(game.board[row][column + 1] === game.oppositeTurn()){
+                piecesToFlip.push([row, column + 1]); 
+                checkMove('right', row, column + 1);
+            } else if(game.board[row][column + 1] === game.currentTurn()){
+                piecesToFlip.forEach(function(element){
+                    game.flipPiece(element[0],element[1]);
+                });
+            }            
+        } else if(direction === 'down'){
+            if(game.board[row - 1][column] === game.oppositeTurn()){
+                piecesToFlip.push([row + 1, column]);
+                checkMove('down', row + 1, column);
+            } else if(game.board[row + 1][column] === game.currentTurn()){
+                piecesToFlip.forEach(function(element){
+                    game.flipPiece(element[0],element[1]);
+                });
+            } 
+        } else if(direction === 'left'){
+            if(game.board[row][column - 1] === game.oppositeTurn()){
+                piecesToFlip.push([row, column - 1]);
+                checkMove('left', row, column - 1);
+            } else if(game.board[row][column - 1] === game.currentTurn()){
+                console.log('ending');
+                piecesToFlip.forEach(function(element){
+                    game.flipPiece(element[0],element[1]);
+                });
+            } 
+        }
+    };
+    checkNeighbors(row, column);
+    return piecesToFlip;
 }
+
+// Valid Move Check - test to see if a valid move
+Othello.prototype.validMove = function(row, column){
+    var piecesToFlip = [];
+    var validMove = false;
+    var game = this;
+    var neighbors = {
+        up:{
+            value:this.board[row - 1] ? this.board[row - 1][column] : null,
+            direction:'up',
+            row:row - 1,
+            column:column
+            },
+        right:{
+            value:this.board[row][column + 1] ? this.board[row][column + 1] : null ,
+            direction:'right',
+            row:row,
+            column:column + 1
+            },
+        down:{
+            value:this.board[row + 1] ? this.board[row + 1][column] : null,
+            direction:'down',
+            row:row + 1,
+            column:column
+        },
+        left:{
+            value:this.board[row][column - 1] ? this.board[row][column - 1] : null, 
+            direction:'left',
+            row:row,
+            column:column - 1
+        }
+    }
+    let checkMove = function(direction, row, column){
+        if(direction === 'up'){
+            if(game.board[row - 1][column] === game.oppositeTurn()){
+                piecesToFlip.push([row - 1, column]);
+                checkMove('up', row - 1, column);
+            } else if(game.board[row - 1][column] === game.currentTurn()){
+                return true;
+            } 
+        } else if(direction === 'right'){
+             if(game.board[row][column + 1] === game.oppositeTurn()){
+                piecesToFlip.push([row, column + 1]); 
+                checkMove('right', row, column + 1);
+            } else if(game.board[row][column + 1] === game.currentTurn()){
+                return true;
+            }       
+        } else if(direction === 'down'){
+            if(game.board[row - 1][column] === game.oppositeTurn()){
+                piecesToFlip.push([row + 1, column]);
+                checkMove('down', row + 1, column);
+            } else if(game.board[row + 1][column] === game.currentTurn()){
+                return true;
+            } 
+        } else if(direction === 'left'){
+            if(game.board[row][column - 1] === game.oppositeTurn()){
+                piecesToFlip.push([row, column - 1]);
+                checkMove('left', row, column - 1);
+            } else if(game.board[row][column - 1] === game.currentTurn()){
+                console.log('ending');
+                return true;
+            } 
+        }
+        return false;
+    };
+
+    for(key in neighbors){
+        console.log(key);
+        console.log(neighbors[key].value);
+        if(neighbors[key].value === game.oppositeTurn()){
+            console.log('move here');
+            piecesToFlip.push([neighbors[key].row, neighbors[key].column]);
+            validMove = checkMove(key, neighbors[key].row, neighbors[key].column) ? true : false;
+        } else {
+            console.log('no move');
+        }
+    }
+    return validMove;
+    // console.log(piecesToFlip);
+    // if(piecesToFlip.length > 0){
+    //     return true;
+    // }
+    // else {
+    //     return false;
+    // } 
+}
+
+
+
 // Putting together all the methods that encorperate 1 turn
 Othello.prototype.takeTurn = function(row, column){
     if(this.board[row][column] === null){
         // place piece
         this.placePiece(row, column);
         // flip pieces
-        // this.flipPiece(row, column);
-        // udpate score
-        this.whitesTurn ? this.whiteScore++ && this.blackScore-- : this.blackScore++ && this.whiteScore--;
+        this.flipAvailable(row, column);
         // update history
         // update newPiece
         // update flipped
--- test/modelTest.js
+++ test/modelTest.js
@@ -1,78 +1,348 @@
 var assert = chai.assert;
-var expect = chai.expect;
-describe('Model', function() {
+    var expect = chai.expect;
+    describe('Model', function() {
+    describe('Properties', function() { 
 
-    it('placing piece should change turn', function() {
-        let testGame = new Othello;
-        testGame.startGame();
-        testGame.takeTurn(0, 0);
-        assert.equal(testGame.whitesTurn, false);
+        it('placing piece should change turn', function() {
+            let testGame = new Othello;
+            testGame.startGame();
+            testGame.takeTurn(0, 0);
+            assert.equal(testGame.whitesTurn, false);
+        }); 
+
+        it('placing piece should update score', function() {
+            // testing for white's first turn
+            let testGame = new Othello;
+            testGame.startGame();
+            testGame.takeTurn(2, 3);
+            assert.equal(testGame.whiteScore, 4);
+            assert.equal(testGame.blackScore, 1);
+            // testing for blacks second turn
+            let testGame2 = new Othello;
+            testGame2.startGame();
+            testGame2.takeTurn(2, 3);
+            testGame2.takeTurn(2, 4);
+            assert.equal(testGame2.whiteScore, 3);
+            assert.equal(testGame2.blackScore, 3);
+        }); 
+
+        it('placing piece should update board', function() {
+            let testGame = new Othello;
+            testGame.startGame();
+            testGame.takeTurn(0, 0);
+            assert.equal(testGame.board[0][0], 'white');
+        }); 
+
+        it('placing piece should NOT update existing pieces', function() {
+            let testGame = new Othello;
+            testGame.startGame();
+            testGame.takeTurn(3, 3);
+            assert.equal(testGame.board[3][3], 'black');
+        }); 
+
+        it('game over - full board should update status to over', function() {
+            let testGame = new Othello;
+            testGame.startGame();
+            // still figuring out what ends the game...
+            assert.equal(testGame.status, 'over');
+        }); 
+
+
+
+
+    }); 
+
+    describe('Helper Methods', function() {
+        it('flip piece - changes it on the board', function() {
+            let testGame = new Othello;
+            testGame.startGame();
+            testGame.board[0][0] = 'white';
+            testGame.flipPiece(0,0);
+            assert.equal(testGame.board[0][0], 'black');
+        }); 
+
+        it('flip available - flips available pieces', function() {
+            let testGame = new Othello;
+            testGame.startGame();
+            testGame.takeTurn(3, 2);
+            assert.equal(testGame.board[3][3], 'white');            
+        });
+        
+        it('placing piece - should update status to active', function() {
+            let testGame = new Othello;
+            testGame.startGame();
+            assert.equal(testGame.status, 'active');
+        }); 
+
+        it('remove piece - should update status to active', function() {
+            let testGame = new Othello;
+            testGame.startGame();
+            assert.equal(testGame.status, 'active');
+        });
+
+        it('valid move - check for valid moves for white', function() {
+            let testGame = new Othello;
+            testGame.startGame();
+            assert.equal(testGame.validMove(0,0), false);
+            assert.equal(testGame.validMove(3,5), false);
+            assert.equal(testGame.validMove(5,4), true);
+            assert.equal(testGame.validMove(3,2), true);
+        });
+
+        it('valid move - check for valid moves for black', function() {
+            let testGame = new Othello;
+            testGame.startGame();
+            testGame.takeTurn(3,2);
+            assert.equal(testGame.validMove(0,0), false);
+            assert.equal(testGame.validMove(3,1), false);
+            assert.equal(testGame.validMove(2,4), true);
+            assert.equal(testGame.validMove(4,2), true);
+
+
+        });
+    }); 
+    
+    describe('Take Turn', function() {  
+        it('correct props on first move', function() {
+            let testGame = new Othello;
+            testGame.startGame();
+            testGame.takeTurn(2,3);          
+            let expected = {
+                whitesTurn:false,
+                status:'active',
+                board:[
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,'white',null,null,null,null],
+                    [null,null,null,'white','white',null,null,null],
+                    [null,null,null,'white','black',null,null,null],
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,null,null,null,null,null]
+                    ],
+                whiteScore:4,
+                blackScore:1,
+                history:[],
+                newPiece:null,
+                flipped:[],
+            };
+            expect(JSON.stringify(testGame)).to.eql(JSON.stringify(expected));
+        });
+
+        it('correct props on second move', function() {
+            let testGame = new Othello;
+            testGame.startGame();
+            testGame.takeTurn(2,3);   
+            testGame.takeTurn(2,4);          
+            let expected = {
+                whitesTurn:true,
+                status:'active',
+                board:[
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,'white','black',null,null,null],
+                    [null,null,null,'white','black',null,null,null],
+                    [null,null,null,'white','black',null,null,null],
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,null,null,null,null,null]
+                    ],
+                whiteScore:3,
+                blackScore:3,
+                history:[],
+                newPiece:null,
+                flipped:[],
+            };
+            expect(JSON.stringify(testGame)).to.eql(JSON.stringify(expected));
+        });
+
+        it('correct props on third move', function() {
+            let testGame = new Othello;
+            testGame.startGame();
+            testGame.takeTurn(2,3);   
+            testGame.takeTurn(2,4);
+            testGame.takeTurn(2,5);          
+            let expected = {
+                whitesTurn:false,
+                status:'active',
+                board:[
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,'white','white','white',null,null],
+                    [null,null,null,'white','black',null,null,null],
+                    [null,null,null,'white','black',null,null,null],
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,null,null,null,null,null]
+                    ],
+                whiteScore:5,
+                blackScore:2,
+                history:[],
+                newPiece:null,
+                flipped:[],
+            };
+            expect(JSON.stringify(testGame)).to.eql(JSON.stringify(expected));
+        });
+
+        it('correct props on fourth move', function() {
+           let testGame = new Othello;
+           testGame.startGame();
+           testGame.takeTurn(2,3);   
+           testGame.takeTurn(2,4);
+           testGame.takeTurn(2,5);          
+           testGame.takeTurn(4,2);  
+           let expected = {
+               whitesTurn:true,
+               status:'active',
+               board:[
+                   [null,null,null,null,null,null,null,null],
+                   [null,null,null,null,null,null,null,null],
+                   [null,null,null,'white','white','white',null,null],
+                   [null,null,null,'white','black',null,null,null],
+                   [null,null,'black','black','black',null,null,null],
+                   [null,null,null,null,null,null,null,null],
+                   [null,null,null,null,null,null,null,null],
+                   [null,null,null,null,null,null,null,null]
+                   ],
+               whiteScore:4,
+               blackScore:4,
+               history:[],
+               newPiece:null,
+               flipped:[],
+           };
+           expect(JSON.stringify(testGame)).to.eql(JSON.stringify(expected));
+        });
+
+        it('correct props on fith move', function() {
+           let testGame = new Othello;
+           testGame.startGame();
+           testGame.takeTurn(2,3);   
+           testGame.takeTurn(2,4);
+           testGame.takeTurn(2,5);          
+           testGame.takeTurn(4,2);  
+           testGame.takeTurn(5,4);  
+
+           let expected = {
+               whitesTurn:false,
+               status:'active',
+               board:[
+                   [null,null,null,null,null,null,null,null],
+                   [null,null,null,null,null,null,null,null],
+                   [null,null,null,'white','white','white',null,null],
+                   [null,null,null,'white','white',null,null,null],
+                   [null,null,'black','black','white',null,null,null],
+                   [null,null,null,null,'white',null,null,null],
+                   [null,null,null,null,null,null,null,null],
+                   [null,null,null,null,null,null,null,null]
+                   ],
+               whiteScore:7,
+               blackScore:2,
+               history:[],
+               newPiece:null,
+               flipped:[],
+           };
+           expect(JSON.stringify(testGame)).to.eql(JSON.stringify(expected));
+        });
+        it('moves on all the edges work', function() {
+           // come back to this one!!!
+           let testGame = new Othello;
+           testGame.startGame();
+           testGame.takeTurn(2,3);   
+           testGame.takeTurn(2,4);
+           testGame.takeTurn(1,3);          
+           testGame.takeTurn(4,2);  
+           testGame.takeTurn(5,4);  
+
+           let expected = {
+               whitesTurn:false,
+               status:'active',
+               board:[
+                   [null,null,null,null,null,null,null,null],
+                   [null,null,null,null,null,null,null,null],
+                   [null,null,null,'white','white','white',null,null],
+                   [null,null,null,'white','white',null,null,null],
+                   [null,null,'black','black','white',null,null,null],
+                   [null,null,null,null,'white',null,null,null],
+                   [null,null,null,null,null,null,null,null],
+                   [null,null,null,null,null,null,null,null]
+                   ],
+               whiteScore:7,
+               blackScore:2,
+               history:[],
+               newPiece:null,
+               flipped:[],
+           };
+           expect(JSON.stringify(testGame)).to.eql(JSON.stringify(expected));
+        });
+        it('cant make a move if no flip available', function() {
+           let testGame = new Othello;
+           testGame.startGame();
+           testGame.takeTurn(0,0);  
+           let expected = {
+               whitesTurn:true,
+               status:'active',
+               board:[
+                   [null,null,null,null,null,null,null,null],
+                   [null,null,null,null,null,null,null,null],
+                   [null,null,null,null,null,null,null,null],
+                   [null,null,null,'black','white',null,null,null],
+                   [null,null,null,'white','black',null,null,null],
+                   [null,null,null,null,null,null,null,null],
+                   [null,null,null,null,null,null,null,null],
+                   [null,null,null,null,null,null,null,null]
+                   ],
+               whiteScore:2,
+               blackScore:2,
+               history:[],
+               newPiece:null,
+               flipped:[],
+           };
+           expect(JSON.stringify(testGame)).to.eql(JSON.stringify(expected));
+        });
+
     });
 
-    it('placing piece should update score', function() {
-        // testing for white's first turn
-        let testGame = new Othello;
-        testGame.startGame();
-        testGame.takeTurn(0, 0);
-        assert.equal(testGame.whiteScore, 3);
-        assert.equal(testGame.blackScore, 1);
-        // testing for blacks second turn
-        let testGame2 = new Othello;
-        testGame2.startGame();
-        testGame2.takeTurn(0, 0);
-        testGame2.takeTurn(0, 1);
-        assert.equal(testGame2.whiteScore, 2);
-        assert.equal(testGame2.blackScore, 2);
+    describe('Flip Available', function() {  
+    
     });
 
-    it('placing piece should update board', function() {
-        let testGame = new Othello;
-        testGame.startGame();
-        testGame.takeTurn(0, 0);
-        assert.equal(testGame.board[0][0], 'white');
+    describe('Undo Turn Method', function() {  
+        //need to implement
+        it('sets current model equal to last element in history', function() {
+            let testGame = new Othello;
+            testGame.startGame();
+            assert.equal(testGame.status, 'active');
+        }); 
     });
 
-    it('placing piece should NOT update existing pieces', function() {
-        let testGame = new Othello;
-        testGame.startGame();
-        testGame.takeTurn(3, 3);
-        assert.equal(testGame.board[3][3], 'black');
-    });
+    describe('Start Game Method', function() {  
 
-    it('full board should update status to over', function() {
-        let testGame = new Othello;
-        testGame.startGame();
-        // still figuring out what ends the game...
-        assert.equal(testGame.status, 'over');
-    });
+        it('starting game - populates model', function() {
+            let testGame = new Othello;
+            testGame.startGame();
+            let expected = {
+                whitesTurn:true,
+                status:'active',
+                board:[
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,'black','white',null,null,null],
+                    [null,null,null,'white','black',null,null,null],
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,null,null,null,null,null],
+                    [null,null,null,null,null,null,null,null]
+                    ],
+                whiteScore:2,
+                blackScore:2,
+                history:[],
+                newPiece:null,
+                flipped:[],
+            };
+            expect(JSON.stringify(testGame)).to.eql(JSON.stringify(expected));
+        });
+    }); 
 
-    it('placing piece should update status to active', function() {
-        let testGame = new Othello;
-        testGame.startGame();
-        assert.equal(testGame.status, 'active');
-    });
-
-    it('starting game should populate model', function() {
-        let testGame = new Othello;
-        testGame.startGame();
-        let expected = {
-            whitesTurn:true,
-            status:'active',
-            board:[[null,null,null,null,null,null,null,null],
-                [null,null,null,null,null,null,null,null],
-                [null,null,null,null,null,null,null,null],
-                [null,null,null,'black','white',null,null,null],
-                [null,null,null,'white','black',null,null,null],
-                [null,null,null,null,null,null,null,null],
-                [null,null,null,null,null,null,null,null],
-                [null,null,null,null,null,null,null,null]
-                ],
-            whiteScore:2,
-            blackScore:2,
-            history:[],
-            newPiece:null,
-            flipped:[],
-        };
-        expect(JSON.stringify(testGame)).to.eql(JSON.stringify(expected));
+    describe('Restart Game', function() {  
     });
 });
